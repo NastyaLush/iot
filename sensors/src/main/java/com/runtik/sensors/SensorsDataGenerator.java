76b9702d7332fbc28bb851ac1ef496a84f1e679a
NastyaLush <yfcn.if1203@gmail.com>
2024-05-09 17:41:05 +0300

    add graphana to sensors

--- sensors/src/main/java/com/runtik/sensors/SensorsDataGenerator.java
+++ sensors/src/main/java/com/runtik/sensors/SensorsDataGenerator.java
@@ -1,28 +1,37 @@
 package com.runtik.sensors;
 
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class SensorsDataGenerator {
     private final RestTemplate restTemplate = new RestTemplate();
     private final Map<UUID, Sensor> sensors = new HashMap<>();
-
+    private final Map<UUID, Double> distributionSummary = new HashMap<>();
+    private final MeterRegistry meterRegistry;
     @PostConstruct
     public void init() {
-        for (int i = 0; i < 100; i++) {
-            UUID id = UUID.randomUUID();
+        for (int i = 0; i < 10; i++) {
+            UUID uuid = UUID.randomUUID();
             Type type = Type.values()[(int) (Math.random() * Type.values().length)];
-            Sensor sensor = new Sensor(id, type.getValue());
-            sensors.put(sensor.id(), sensor);
+            Sensor sensor = new Sensor(uuid, type.getValue());
+            sensors.put(uuid, sensor);
+            distributionSummary.put(sensor.id(), 0d);
+            Gauge.builder(sensor.id().toString(), distributionSummary, (distributionSummar)->distributionSummary.get(sensor.id())).register(meterRegistry);
             // Register sensor
             restTemplate.postForObject("http://localhost:8082/sensor",
-                    new Sensor(id, type.getValue()), Void.class);
+                    new Sensor(uuid, type.getValue()), Void.class);
         }
 
     }
@@ -31,7 +40,7 @@ public class SensorsDataGenerator {
     public void sendData() {
         for (Sensor sensor : sensors.values()) {
             Double sensorData = restTemplate.getForObject("http://localhost:8080/" + sensor.type(), Double.class);
-
+            distributionSummary.put(sensor.id(), sensorData);
             // Send data to adapter asynchronously
             restTemplate.postForObject("http://localhost:8081/adapter",
                     new Message(sensor.id()
