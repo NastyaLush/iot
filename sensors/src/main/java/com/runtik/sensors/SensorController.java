9900ef2b29dd802cbeb4eecbafcbb6b80213bfaa
NastyaLush <yfcn.if1203@gmail.com>
2024-05-09 15:34:26 +0300

    add sensor delete endpoint

--- sensors/src/main/java/com/runtik/sensors/SensorController.java
+++ sensors/src/main/java/com/runtik/sensors/SensorController.java
@@ -1,4 +1,18 @@
 package com.runtik.sensors;
 
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class SensorController {
+    private final SensorsDataGenerator sensorsDataGenerator;
+
+    @DeleteMapping
+    public void delete(@RequestParam UUID id) {
+        sensorsDataGenerator.deleteSensor(id);
+    }
 }
--- sensors/src/main/java/com/runtik/sensors/SensorsDataGenerator.java
+++ sensors/src/main/java/com/runtik/sensors/SensorsDataGenerator.java
@@ -1,27 +1,25 @@
 package com.runtik.sensors;
 
 import jakarta.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
-import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SensorsDataGenerator {
     private final RestTemplate restTemplate = new RestTemplate();
-    private final List<Sensor> sensors = new ArrayList<>();
+    private final Map<UUID, Sensor> sensors = new HashMap<>();
 
     @PostConstruct
     public void init() {
-        for(int i=0;i<100;i++){
+        for (int i = 0; i < 100; i++) {
             UUID id = UUID.randomUUID();
             Type type = Type.values()[(int) (Math.random() * Type.values().length)];
             Sensor sensor = new Sensor(id, type.getValue());
-            sensors.add(sensor);
+            sensors.put(sensor.id(), sensor);
             // Register sensor
             restTemplate.postForObject("http://localhost:8082/sensor",
                     new Sensor(id, type.getValue()), Void.class);
@@ -31,14 +29,19 @@ public class SensorsDataGenerator {
 
     @Scheduled(fixedRate = 10000) // Send data every 10 seconds
     public void sendData() {
-        for(Sensor sensor : sensors) {
+        for (Sensor sensor : sensors.values()) {
             Double sensorData = restTemplate.getForObject("http://localhost:8080/" + sensor.type(), Double.class);
 
             // Send data to adapter asynchronously
             restTemplate.postForObject("http://localhost:8081/adapter",
-                    new Message(sensor.id().toString(), sensorData), Void.class);
+                    new Message(sensor.id()
+                                      .toString(), sensorData), Void.class);
 
         }
 
+    }
+
+    public void deleteSensor(UUID sensorId) {
+        sensors.remove(sensorId);
     }
 }
